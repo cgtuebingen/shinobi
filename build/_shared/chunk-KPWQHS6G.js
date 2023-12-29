bf2d0f23369497c17f40efeb5fb40fe6642acd2e
JDihlmann <JDihlmann@users.noreply.github.com>
2023-12-29 09:09:34 +0000

    deploy: 4837c7f809cb7847c01b9a9fd41c3166fe63ae85

--- build/_shared/chunk-KPWQHS6G.js
+++
@@ -1,1 +0,0 @@
-import{b as o}from"/build/_shared/chunk-T36URGAI.js";var p=o((r,e)=>{e.exports={}});export{p as a};
--- build/root-IMG7T2SC.js
+++
@@ -1,9 +0,0 @@
-import{a as f}from"/build/_shared/chunk-KPWQHS6G.js";import{a,b as i,d as n,e as c,f as s,g as p,h as d,j as l}from"/build/_shared/chunk-D7TEDUQN.js";import{a as y,b as w}from"/build/_shared/chunk-3Z32TANI.js";import{d as r}from"/build/_shared/chunk-T36URGAI.js";var x=r(f(),1);var g="/build/_assets/app-NRS5MYHR.css";var b=(e,o)=>{if(!window.gtag){console.warn("window.gtag is not defined. This could mean your google analytics script has not loaded on the page yet.");return}window.gtag("config",o,{page_path:e})};var h=r(y(),1),t=r(w(),1),k=()=>[{rel:"stylesheet",href:g},{rel:"stylesheet",href:"https://rsms.me/inter/inter.css"}];function m(){let e=a(),{title:o}=p();return(0,h.useEffect)(()=>{b(e.pathname,"G-H3WNEKDGDG")},[e]),(0,t.jsxs)("html",{lang:"en",children:[(0,t.jsxs)("head",{children:[(0,t.jsx)("meta",{charSet:"utf-8"}),(0,t.jsx)("meta",{name:"viewport",content:"width=device-width, initial-scale=1"}),(0,t.jsx)("link",{rel:"icon",href:"favicon.ico"}),(0,t.jsx)(c,{}),(0,t.jsx)(n,{}),(0,t.jsx)("title",{children:o.title}),(0,t.jsx)("script",{src:"babylon/babylon.js"}),(0,t.jsx)("script",{src:"babylon/babylonjs.loaders.min.js"}),(0,t.jsx)("script",{src:"babylon/babylon.gui.min.js"}),(0,t.jsx)("script",{src:"https://code.jquery.com/pep/0.4.3/pep.js"})]}),(0,t.jsxs)("body",{style:{overscrollBehavior:"none"},children:[(0,t.jsx)("script",{async:!0,src:"https://www.googletagmanager.com/gtag/js?id=G-H3WNEKDGDG"}),(0,t.jsx)("script",{async:!0,id:"gtag-init",dangerouslySetInnerHTML:{__html:`
-								window.dataLayer = window.dataLayer || [];
-								function gtag(){dataLayer.push(arguments);}
-								gtag('js', new Date());
-
-								gtag('config', 'G-H3WNEKDGDG', {
-								page_path: window.location.pathname,
-								});
-							`}}),(0,t.jsx)(i,{}),(0,t.jsx)(l,{}),(0,t.jsx)(s,{}),(0,t.jsx)(d,{})]})]})}export{m as default,k as links};
